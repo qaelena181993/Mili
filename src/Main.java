c4f121949a1bd6a0cdd83ca3915abbd76a45cabf
Chaia Elena <qaelena181993@gmail.com>
2022-06-29 23:37:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // входные данные
+        short amount = 12000;
+        byte mile = 20;
+
+        // логика программы
+        int bonus = amount / mile;
+        System.out.println("Ваш бонус составит: " + bonus);
+    }
+
+}
+
+
+
+
